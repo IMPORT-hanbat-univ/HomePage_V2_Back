54cfd75c022146ef8ea2a8b0ae33c4b12165706d
devdubu <insomnis2020@naver.com>
2022-07-23 13:42:06 +0900

    Start Spring

---
+++ src/main/java/com/example/demo/HomePageV2BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomePageV2BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomePageV2BackApplication.class, args);
+	}
+
+}
